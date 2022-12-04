6a8bce7ec342fff2289f1c70ab66b418d409fa25
thienle77 <tle9677@gmail.com>
2022-12-04 18:25:37 -0500

    wip

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -4,4 +4,20 @@ package com.zipcodewilmington.arrayutility;
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility {
+
+    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+        return valueToEvaluate;
+    }
+
+    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+        return valueToEvaluate;
+    }
+
+    public Long[] removeValue(Long valueToRemove) {
+        return new Long[0];
+    }
 }
